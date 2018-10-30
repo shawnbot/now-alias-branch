e056715ce487601795b9feba1dff9a19f9e1444b
Shawn Allen <shawn.allen@github.com>
2018-10-29 22:35:24 -0700

    interpolations everywhere!

--- cli.js
+++ cli.js
@@ -14,8 +14,8 @@ const {
   NOW_APP_NAME: app = require(NOW_JSON_PATH).name,
   PREVIEW_URL_TEMPLATE = '{app}-{branch}.now.sh',
   ROOT_URL = fs.readFileSync('/github/workflow/zeit-now.log', 'utf8').trim(),
-  STATUS_CONTEXT = 'now/preview',
-  STATUS_DESCRIPTION = 'Your preview is up-to-date'
+  STATUS_CONTEXT = 'preview/url',
+  STATUS_DESCRIPTION = 'Your preview is up-to-date with {sha}'
 } = process.env
 
 const event = require(GITHUB_EVENT_PATH)
@@ -24,32 +24,36 @@ const rootDomain = url.parse(ROOT_URL).host
 getRelevantDeployment(rootDomain).then(deployment => {
   console.warn('relevant deployment:', deployment)
   const branch = event.ref.split('/').slice(2).join('/')
-  const url = interpolate(PREVIEW_URL_TEMPLATE, {
-    branch,
+  const data = {
     app,
-    deployment
-  })
-  console.warn('alias url:', url)
+    repo: GITHUB_REPOSITORY,
+    sha: GITHUB_SHA,
+    branch,
+    uid: deployment.uid
+  }
+  const host = interpolate(PREVIEW_URL_TEMPLATE, data)
+  console.warn('alias host:', host)
   return nowFetch(`/v2/now/deployments/${deployment.uid}/aliases`, {
     method: 'post',
-    body: JSON.stringify({alias: url}),
+    body: JSON.stringify({alias: host}),
     headers: {
       'content-type': 'application/json'
     }
   })
   .catch(err => {
-    throw new Error(`Unable to alias deployment: "${deployment.uid}" to "${url}": ${error}`)
+    throw new Error(`Unable to alias deployment: "${deployment.uid}" to "${host}": ${error}`)
   })
   .then(() => {
     const [owner, repo] = GITHUB_REPOSITORY.split('/')
+    data.host = host
     const payload = {
       owner,
       repo,
       sha: GITHUB_SHA,
       state: 'success',
-      context: STATUS_CONTEXT,
-      description: STATUS_DESCRIPTION,
-      target_url: `https://${url}`
+      context: interpolate(STATUS_CONTEXT, data),
+      description: interpolate(STATUS_DESCRIPTION, data),
+      target_url: `https://${host}`
     }
     console.log('status payload:', payload)
     return github.repos.createStatus(payload)
