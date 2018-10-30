643073a29c4e04ee668b3b49425b111416f8104b
Shawn Allen <shawn.allen@github.com>
2018-10-29 22:04:25 -0700

    use more env vars

--- cli.js
+++ cli.js
@@ -4,31 +4,26 @@ const interpolate = require('interpolate')
 const url = require('url')
 const {join} = require('path')
 const github = require('./src/octokit')
-const getEventData = require('./src/event')
 const {getRelevantDeployment} = require('./src/now')
 
 const {
-  GITHUB_EVENT_PATH,
+  GITHUB_EVENT_PATH = require.resolve('./event.json'),
+  GITHUB_REPOSITORY,
+  GITHUB_SHA,
   NOW_JSON_PATH = join(process.cwd(), 'now.json'),
-  NOW_APP_NAME = require(NOW_JSON_PATH).name,
+  NOW_APP_NAME: app = require(NOW_JSON_PATH).name,
   PREVIEW_URL_TEMPLATE = '{app}-{branch}',
   ROOT_URL = fs.readFileSync('/github/workflow/zeit-now.log', 'utf8').trim(),
   STATUS_CONTEXT = 'now/preview',
   STATUS_DESCRIPTION = 'Your preview is up-to-date'
 } = process.env
 
-const eventPath = GITHUB_EVENT_PATH || require.resolve('./event.json')
-const event = require(eventPath)
-
-const {branch, ...data} = getEventData(event)
-const app = NOW_APP_NAME || require('./now.json').name
-
-const rootUrl = ROOT_URL || fs.readFileSync('/zeit-now.log', 'utf8').trim()
-const rootDomain = url.parse(rootUrl).host
+const event = require(GITHUB_EVENT_PATH)
+const rootDomain = url.parse(ROOT_URL).host
 
 getRelevantDeployment(rootDomain).then(deployment => {
-  const interps = Object.assign({}, deployment, data)
-  const url = interpolate(PREVIEW_URL_TEMPLATE, interps)
+  const branch = event.ref.split('/').slice(2).join('/')
+  const url = interpolate(PREVIEW_URL_TEMPLATE, {branch, app})
   console.warn('alias url:', url)
   return nowFetch(`/v2/now/deployments/${deployment.uid}/aliases`, {
     method: 'post',
