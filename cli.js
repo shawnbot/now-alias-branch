e106c6be6cc6014ad43c687484d74effd26f5c64
Shawn Allen <shawn.allen@github.com>
2018-10-29 20:35:54 -0700

    derp

--- cli.js
+++ cli.js
@@ -7,13 +7,14 @@ console.log('event:', JSON.stringify(event, null, 2))
 Promise.all([
   execa('git', ['rev-parse', '--short', 'HEAD']).then(res => res.stdout),
   execa('git', ['symbolic-ref', '--short', 'HEAD']).then(res => res.stdout),
-  event.repository.owner.name,
-  event.repository.name
 ])
 .catch(error => {
   console.error(error)
   process.exitCode = 1
 })
-.then(([ref, branch, owner, repo]) => {
+.then(results => {
+  const [ref, branch] = results
+  const owner = event.repository.owner.name
+  const repo = event.repository.name
   console.log('!', {ref, branch, owner, repo})
 })
