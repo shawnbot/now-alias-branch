74fba84dbd800db0ef9a43614362f691f42e2301
Shawn Allen <shawn.allen@github.com>
2018-10-29 20:32:10 -0700

    get the owner and repo name right

--- cli.js
+++ cli.js
@@ -7,12 +7,13 @@ console.log('event:', JSON.stringify(event, null, 2))
 Promise.all([
   execa('git', ['rev-parse', '--short', 'HEAD']).then(res => res.stdout),
   execa('git', ['symbolic-ref', '--short', 'HEAD']).then(res => res.stdout),
-  event.repository.split('/', 2)
+  event.repository.owner.name,
+  event.repository.name
 ])
 .catch(error => {
   console.error(error)
   process.exitCode = 1
 })
-.then(([ref, branch, [owner, repo]]) => {
-  console.log('gotcha:', {ref, branch, owner, repo})
+.then(([ref, branch, owner, repo]) => {
+  console.log('!', {ref, branch, owner, repo})
 })
